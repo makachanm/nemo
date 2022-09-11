7a0c425eaff11b9a3bee95536395dd0996a303b0
Mincart8925 <minecart8926@daum.net>
2022-09-11 18:32:53 +0900

    add cli interface

--- cli/cli.go
+++ cli/cli.go
@@ -1,12 +1,30 @@
 package cli
 
+import (
+	"fmt"
+	"nemo/build"
+)
+
 type CliInterface struct {
 }
 
-func MakeCliInterface() CliInterface {
+func MakeCli() CliInterface {
 	return CliInterface{}
 }
 
-func (ci *CliInterface) Handle() {
+func (ci *CliInterface) Handle(args []string) {
+	if len(args) < 2 {
+		fmt.Println("not enough arguments")
+		return
+	}
 
+	switch args[1] {
+	case "build":
+		build_handler()
+	}
+}
+
+func build_handler() {
+	b := build.MakeNewBuilder()
+	b.Build()
 }
--- main.go
+++ main.go
@@ -1,1 +1,12 @@
 package main
+
+import (
+	"nemo/cli"
+	"os"
+)
+
+func main() {
+	app := cli.MakeCli()
+	app.Handle(os.Args)
+	//os.Args
+}
