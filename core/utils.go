093a741700221126bb4152a33bbab2a4114864ed
caipira113 <caipira@libnare.net>
2023-04-04 02:25:51 +0900

    Update utils.go

--- core/utils.go
+++ core/utils.go
@@ -3,100 +3,76 @@ package build
 import (
 	"fmt"
 	"io"
-	"log"
 	"os"
 	"path/filepath"
 )
 
-func DirCopy(src string, dst string) error {
-	// Get properties of source dir
+const bufferSize = 32 * 1024
+
+func DirCopy(src, dst string) error {
 	srcInfo, err := os.Stat(src)
 	if err != nil {
-		return err
+		return fmt.Errorf("failed to get source directory properties: %w", err)
 	}
-
-	// Check if src and dst directories are the same
 	if src == dst {
 		return fmt.Errorf("source and destination directories are the same")
 	}
-
-	// Create destination dir
-	err = os.MkdirAll(dst, srcInfo.Mode())
-	if err != nil {
-		return err
+	if err := os.MkdirAll(dst, srcInfo.Mode()); err != nil {
+		return fmt.Errorf("failed to create destination directory: %w", err)
 	}
-
 	directory, err := os.Open(src)
 	if err != nil {
-		return err
+		return fmt.Errorf("failed to open source directory: %w", err)
 	}
 	defer func(directory *os.File) {
-		err := directory.Close()
-		if err != nil {
-			log.Fatal(err)
-		}
+		_ = directory.Close()
 	}(directory)
-
 	objects, err := directory.Readdir(-1)
 	if err != nil {
-		return err
+		return fmt.Errorf("failed to read source directory contents: %w", err)
 	}
-
 	for _, obj := range objects {
-		srcPointer := src + string(filepath.Separator) + obj.Name()
-		dstPointer := dst + string(filepath.Separator) + obj.Name()
-
+		srcPointer := filepath.Join(src, obj.Name())
+		dstPointer := filepath.Join(dst, obj.Name())
 		if obj.IsDir() {
-			// Create sub-directories - recursively
-			err = DirCopy(srcPointer, dstPointer)
-			if err != nil {
-				return err
+			if err := DirCopy(srcPointer, dstPointer); err != nil {
+				return fmt.Errorf("failed to copy sub-directory %s: %w", srcPointer, err)
 			}
 		} else {
-			// Perform copy
-			err = FileCopy(srcPointer, dstPointer)
-			if err != nil {
-				return err
+			if err := FileCopy(srcPointer, dstPointer); err != nil {
+				return fmt.Errorf("failed to copy file %s: %w", srcPointer, err)
 			}
 		}
-
 	}
 	return nil
 }
 
-func FileCopy(src string, dst string) error {
+func FileCopy(src, dst string) error {
 	sourceFileStat, err := os.Stat(src)
 	if err != nil {
-		return err
+		return fmt.Errorf("failed to get source file properties: %w", err)
 	}
-
 	if !sourceFileStat.Mode().IsRegular() {
 		return fmt.Errorf("%s is not a regular file", src)
 	}
-
 	source, err := os.Open(src)
 	if err != nil {
-		return err
+		return fmt.Errorf("failed to open source file: %w", err)
 	}
 	defer func(source *os.File) {
-		err := source.Close()
-		if err != nil {
-			log.Fatal(err)
-		}
+		_ = source.Close()
 	}(source)
-
 	destination, err := os.Create(dst)
 	if err != nil {
-		return err
+		return fmt.Errorf("failed to create destination file: %w", err)
 	}
 	defer func(destination *os.File) {
-		err := destination.Close()
-		if err != nil {
-			log.Fatal(err)
-		}
+		_ = destination.Close()
 	}(destination)
-
-	buf := make([]byte, 32*1024)
+	buf := make([]byte, bufferSize)
 	_, err = io.CopyBuffer(destination, source, buf)
-	return err
+	if err != nil {
+		return fmt.Errorf("failed to copy file contents: %w", err)
+	}
+	return nil
 }
