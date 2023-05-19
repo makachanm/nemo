17be6a12fcccc82c7e0dd5f36826df4d1c01ff4a
Mincart8925 <minecart8926@daum.net>
2023-05-20 00:35:01 +0900

    build: sperate timestamp struct from document

--- core/document.go
+++ core/document.go
@@ -10,14 +10,6 @@ import (
 
 type MarkdownFucntion nmcore.MarkdownFucntion
 
-type TimeStamp struct {
-	Year  int
-	Month int
-	Day   int
-	Hour  int
-	Min   int
-}
-
 type DocumentMeta struct {
 	Title     string
 	Timestamp TimeStamp
@@ -99,38 +91,6 @@ func NewAboutPage() AboutPage {
 
 func NewTagsData() TagsPage {
 	return TagsPage{}
-}
-
-func MakeTimeStamp(year int, month int, day int, hour int, min int) TimeStamp {
-	return TimeStamp{
-		Year:  year,
-		Month: month,
-		Day:   day,
-		Hour:  hour,
-		Min:   min,
-	}
-}
-
-func (t *TimeStamp) StampSize() int {
-	return t.Year + t.Month + t.Day + t.Hour + t.Min
-}
-
-func (t *TimeStamp) isBiggerStamp(src TimeStamp, cmp TimeStamp) bool {
-	if src.Year > cmp.Year {
-		return true
-	} else if src.Month > cmp.Month {
-		return true
-	} else if src.Month == cmp.Month {
-		if src.Day > cmp.Day {
-			return true
-		} else if src.Hour > cmp.Hour {
-			return true
-		} else if src.Min > cmp.Min {
-			return true
-		}
-	}
-
-	return false
 }
 
 func (d *Document) ParseMeta(input string) {
